5f974e878bc5f019e948c888407084a33b332e83
Mateuss18 <mateusdfl18@gmail.com>
2021-12-18 23:38:11 -0300

    Animação scroll menu

---
+++ script.js
@@ -0,0 +1,58 @@
+//Fazendo o scroll do menu de navegação mais suave
+const intensMenu = document.querySelectorAll('.menu a[href^="#"]')
+
+intensMenu.forEach(item => {
+    item.addEventListener('click', scrollToIdOnClick)
+})
+
+function getScrollTopByHref(element){
+    const id = element.getAttribute('href')
+    return document.querySelector(id).offsetTop;
+}
+
+function scrollToIdOnClick(event){
+    event.preventDefault();
+    const to = getScrollTopByHref(event.target)
+
+    scrollToPosition(to)
+}
+
+function scrollToPosition(to){
+    window.scroll({
+        top: to,
+        behavior: "smooth",
+    })
+    //smoothScrollTo(0, to);
+}
+
+/**
+ * Smooth scroll animation para navegadores que não suportam
+ * @param {int} endX: destination x coordinate
+ * @param {int} endY: destination y coordinate
+ * @param {int} duration: animation duration in ms
+ */
+function smoothScrollTo(endX, endY, duration) {
+  const startX = window.scrollX || window.pageXOffset;
+  const startY = window.scrollY || window.pageYOffset;
+  const distanceX = endX - startX;
+  const distanceY = endY - startY;
+  const startTime = new Date().getTime();
+
+  duration = typeof duration !== 'undefined' ? duration : 400;
+
+  // Easing function
+  const easeInOutQuart = (time, from, distance, duration) => {
+    if ((time /= duration / 2) < 1) return distance / 2 * time * time * time * time + from;
+    return -distance / 2 * ((time -= 2) * time * time * time - 2) + from;
+  };
+
+  const timer = setInterval(() => {
+    const time = new Date().getTime() - startTime;
+    const newX = easeInOutQuart(time, startX, distanceX, duration);
+    const newY = easeInOutQuart(time, startY, distanceY, duration);
+    if (time >= duration) {
+      clearInterval(timer);
+    }
+    window.scroll(newX, newY);
+  }, 1000 / 60); // 60 fps
+};
