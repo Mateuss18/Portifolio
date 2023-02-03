02c2e1aa650d75b2ec7cff83741dc294f5bf2c36
MateusLopes <mateus20.lopes02@gmail.com>
2023-02-02 23:10:56 -0300

    feat: add refatoracao no codigo

--- js/script.js
+++ js/script.js
@@ -1,26 +1,26 @@
 //Fazendo o scroll do menu de navegação mais suave
 const intensMenu = document.querySelectorAll('#menu a[href^="#"]')
 
-intensMenu.forEach(item => {
+intensMenu.forEach((item) => {
     item.addEventListener('click', scrollToIdOnClick)
 })
 
-function getScrollTopByHref(element){
+function getScrollTopByHref(element) {
     const id = element.getAttribute('href')
-    return document.querySelector(id).offsetTop;
+    return document.querySelector(id).offsetTop
 }
 
-function scrollToIdOnClick(event){
-    event.preventDefault();
+function scrollToIdOnClick(event) {
+    event.preventDefault()
     const to = getScrollTopByHref(event.target)
 
     scrollToPosition(to)
 }
 
-function scrollToPosition(to){
+function scrollToPosition(to) {
     window.scroll({
         top: to,
-        behavior: "smooth",
+        behavior: 'smooth'
     })
     //smoothScrollTo(0, to);
 }
@@ -32,39 +32,39 @@ function scrollToPosition(to){
  * @param {int} duration: animation duration in ms
  */
 function smoothScrollTo(endX, endY, duration) {
-  const startX = window.scrollX || window.pageXOffset;
-  const startY = window.scrollY || window.pageYOffset;
-  const distanceX = endX - startX;
-  const distanceY = endY - startY;
-  const startTime = new Date().getTime();
+    const startX = window.scrollX || window.pageXOffset
+    const startY = window.scrollY || window.pageYOffset
+    const distanceX = endX - startX
+    const distanceY = endY - startY
+    const startTime = new Date().getTime()
 
-  duration = typeof duration !== 'undefined' ? duration : 400;
+    duration = typeof duration !== 'undefined' ? duration : 400
 
-  // Easing function
-  const easeInOutQuart = (time, from, distance, duration) => {
-    if ((time /= duration / 2) < 1) return distance / 2 * time * time * time * time + from;
-    return -distance / 2 * ((time -= 2) * time * time * time - 2) + from;
-  };
-
-  const timer = setInterval(() => {
-    const time = new Date().getTime() - startTime;
-    const newX = easeInOutQuart(time, startX, distanceX, duration);
-    const newY = easeInOutQuart(time, startY, distanceY, duration);
-    if (time >= duration) {
-      clearInterval(timer);
+    // Easing function
+    const easeInOutQuart = (time, from, distance, duration) => {
+        if ((time /= duration / 2) < 1)
+            return (distance / 2) * time * time * time * time + from
+        return (-distance / 2) * ((time -= 2) * time * time * time - 2) + from
     }
-    window.scroll(newX, newY);
-  }, 1000 / 60); // 60 fps
-};
+
+    const timer = setInterval(() => {
+        const time = new Date().getTime() - startTime
+        const newX = easeInOutQuart(time, startX, distanceX, duration)
+        const newY = easeInOutQuart(time, startY, distanceY, duration)
+        if (time >= duration) {
+            clearInterval(timer)
+        }
+        window.scroll(newX, newY)
+    }, 1000 / 60) // 60 fps
+}
 
 //Efeito maquina de escrever da area apresentação
-function typeWriter(elemento){
-  const textoArray = elemento.innerHTML.split('')
-  elemento.innerHTML = ''
-  textoArray.forEach((letra, i) => {
-    setTimeout(()=>
-      elemento.innerHTML += letra, 75 * i)
-  })
+function typeWriter(elemento) {
+    const textoArray = elemento.innerHTML.split('')
+    elemento.innerHTML = ''
+    textoArray.forEach((letra, i) => {
+        setTimeout(() => (elemento.innerHTML += letra), 75 * i)
+    })
 }
 
 const titulo = document.getElementById('nome')
@@ -72,23 +72,23 @@ typeWriter(titulo)
 
 //Animação dos elementos aparecendo
 const sr = ScrollReveal({
-  origin: 'top',
-  distance: '50px',
-  duration: 2000,
-  reset: false
-});
+    origin: 'top',
+    distance: '50px',
+    duration: 2000,
+    reset: false
+})
 
-sr.reveal('.image', { delay: 400 });
-sr.reveal('.informacoes', { delay: 400 });
-sr.reveal('.title', { delay: 400 });
-sr.reveal('.description', { delay: 600 });
-sr.reveal('.cardsInterval', { interval: 300 });
+sr.reveal('.image', { delay: 400 })
+sr.reveal('.informacoes', { delay: 400 })
+sr.reveal('.title', { delay: 400 })
+sr.reveal('.description', { delay: 600 })
+sr.reveal('.cardsInterval', { interval: 300 })
 
 //Botão para menu mobile
 const btnMobile = document.getElementById('btn-mobile')
 
-function toggleMenu(event){
-    if(event.type === 'touchstart') event.preventDefault()
+function toggleMenu(event) {
+    if (event.type === 'touchstart') event.preventDefault()
     const nav = document.getElementById('nav')
     nav.classList.toggle('active')
 }
@@ -98,18 +98,15 @@ btnMobile.addEventListener('touchstart', toggleMenu)
 
 //Modal com os projetos
 
-function abrirModal(){
-    let modal = document.querySelector('.modal')
-    let body = document.querySelector('.body')
+const modal = document.querySelector('.modal')
+const body = document.querySelector('.body')
 
+function abrirModal() {
     modal.style.display = 'block'
     body.style.overflow = 'hidden'
 }
 
-function fecharModal(){
-    let modal = document.querySelector('.modal')
-    let body = document.querySelector('.body')
-        
+function fecharModal() {
     modal.style.display = 'none'
     body.style.overflow = 'auto'
 }
